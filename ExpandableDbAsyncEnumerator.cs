533d8a4af2cba569519405456893a8c02e598861
Tore Aurstad <tore.aurstad@hemit.no>
2021-02-05 23:39:49 +0100

    init commit

---
+++ ExpandableDbAsyncEnumerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqKit.AsyncSupport
+{
+
+    public class ExpandableDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>, IDbAsyncEnumerator
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public ExpandableDbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+
+}
